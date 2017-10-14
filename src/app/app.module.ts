2bfb14fb662af6be906319faedd63b83a456ea0c
Karl <karlanuetzel@gmail.com>
2017-10-14 14:26:08 -0500

    added d3 dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule, ApplicationRef } from '@angular/core';
 import {AppComponent} from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { D3Service } from 'd3-ng2-service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [D3Service], // <-- provider registration
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
